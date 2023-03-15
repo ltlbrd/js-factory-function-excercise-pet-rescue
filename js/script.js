d2b770961db31c094887ad9f200723683d7d3dff
Madrona <bdwpdx@gmail.com>
2023-03-15 13:49:31 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const statusButton = document.querySelector("button");
+const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species) {
+    const pet = {
+        name: name,
+        species: species,
+        isTired: 5,
+        sleep: function () {
+            console.log(`${this.name} needs a nap. zzz...`);
+            this.isTired = 1;
+        },
+        play: function () {
+            if (this.isTired === 10) {
+                console.log("Too tired to play");
+                this.sleep();
+            } else {
+                console.log(`Yay! ${this.name} loves to play!`);
+                this.isTired += 1;
+            }
+        }
+    };
+    
+    return pet;
+};
+
+
+const fiona = createPet("Fiona", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Frnacine", "turtle");
+
+console.log(fiona, clover, baxter, cleo, francine);
+
+clover.sleep();
+baxter.play();
+
+console.log(clover, baxter);
+
+
